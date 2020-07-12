ea982c4d7f08e022408a04e7730b89a2ac5ec237
SajZ <sajida.zouarhi@gmail.com>
2020-07-12 13:16:01 +0100

    add files
    Added /src folder of React app

---
+++ src/ipfs.js
@@ -0,0 +1,3 @@
+//using the infura.io node, otherwise ipfs requires you to run a //daemon on your own computer/server.
+const IPFS = require('ipfs-api');
+const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
